e6eaf440b451a4b1f20331598673e3ff2f58691e
Gonzalo G. <gonzalogarcia035@gmail.com>
2025-04-27 18:05:50 -0300

    se crea prompt y bucle para ingresar ciudades

---
+++ js/02-Arrays.js
@@ -0,0 +1,24 @@
+const ciudades = [];
+    let ciudad;
+
+    while (true) {
+        ciudad = prompt("Ingrese una ciudad (o presione Cancelar para terminar):");
+        if (ciudad === null) {
+            break;
+        }
+        ciudades.push(ciudad);
+    }
+    document.writeln("<br>Cantidad de ciudades: " + ciudades.length);
+
+
+
+    document.writeln("<h2>Lista de ciudades</h2>");
+    document.writeln(`<ul>`);
+    for (let i = 0; i < ciudades.length; i++) {
+      document.writeln(`<li>${ciudades[i]}</li>`);
+    }
+    console.log(ciudades)
+
+
+
+
