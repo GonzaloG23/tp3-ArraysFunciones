4dde4d4398ec3252c8a52c00f15955d4440b5f86
Gonzalo G. <gonzalogarcia035@gmail.com>
2025-04-27 21:07:04 -0300

    se crea funcion, se define variable constante y se llama a la funcion

---
+++ js/04-Funciones.js
@@ -0,0 +1,11 @@
+function parImpar(numero){
+    if(numero%2===0)
+        {document.writeln("El numero es par");
+    } else {
+document.writeln("el numero es impar");}
+}
+
+const numeroIngresado = parseInt(prompt("Ingrese un numero"));
+
+parImpar(numeroIngresado)
+
